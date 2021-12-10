84b27162dbab25088554c170d5d22c5d85de7f65
MaximeVerger <vergermaxime.sio@gmail.com>
2021-12-10 14:18:56 +0100

    initial project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button id="nfc">Permission NFC</button>
+    <p>
+        <button onclick="readTag()">Test NFC Read</button>
+        <button onclick="writeTag()">Test NFC Write</button>
+    </p>
+    <pre id="log"></pre>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+window.addEventListener("load", function() {
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+    function displayOutcome(type, outcome) {
+        return function() {
+            var argList = [outcome, type].concat([].slice.call(arguments));
+            switch(outcome) {
+            case "success":
+                console.info.apply(console, argList);
+                break;
+            case "error":
+                console.error.apply(console, argList);
+                break;
+            default:
+                console.log.apply(console, argList);
+            }
+            document.getElementById(type).classList.remove('success', 'error', 'default');
+            document.getElementById(type).classList.add(outcome);
+        };
+    };
+
+    var register = {
+        "nfc": function() {
+            if ('NDEFReader' in window) {
+            const reader = new NDEFReader();
+            reader.scan()
+            .then(() => {
+                displayOutcome("nfc", "success")("Successfully started NFC scan");
+            })
+            .catch((err) => {
+                displayOutcome("nfc", "error")(err);
+            });
+            } else {
+            displayOutcome("nfc", "error")("NDEFReader is not available");
+            }
+        }
+    };
+    
+    document.getElementById("nfc").addEventListener('click', register["nfc"]);
+    
+});
+
+async function readTag() {
+    if ("NDEFReader" in window) {
+        const ndef = new NDEFReader();
+        try {
+        await ndef.scan();
+        ndef.onreading = event => {
+            const decoder = new TextDecoder();
+            for (const record of event.message.records) {
+            consoleLog("Record type:  " + record.recordType);
+            consoleLog("MIME type:    " + record.mediaType);
+            consoleLog("=== data ===\n" + decoder.decode(record.data));
+            }
+        }
+        } catch(error) {
+        consoleLog(error);
+        }
+    } else {
+        consoleLog("Web NFC is not supported.");
+    }
+}
+
+async function writeTag() {
+    if ("NDEFReader" in window) {
+        const ndef = new NDEFReader();
+        try {
+        await ndef.write("What Web Can Do Today");
+        consoleLog("NDEF message written!");
+        } catch(error) {
+        consoleLog(error);
+        }
+    } else {
+        consoleLog("Web NFC is not supported.");
+    }
+}
+
+function consoleLog(data) {
+    var logElement = document.getElementById('log');
+    logElement.innerHTML += data + '\n';
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
