7854b81855d48f09dcc577a00c81214978c20c8e
Axelle HUMBERT <humbertaxelle40@gmail.com>
2021-12-16 16:23:47 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -99,6 +99,7 @@ export default {
         }
     },
     printData(data){
+      this.consoleLog("ça marche");
       var parsed = data.split(" ");
       var Element = document.getElementById(parsed[0]);
       Element.innerHTML += parsed[1]
