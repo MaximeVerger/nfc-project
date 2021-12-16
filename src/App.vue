8d92426dd9bc487b51328cb5fe179a81bbefbbf1
MaximeVerger <vergermaxime.sio@gmail.com>
2021-12-16 09:35:39 +0100

    ??

--- src/App.vue
+++ src/App.vue
@@ -49,12 +49,16 @@ export default {
               await ndef.scan();
               ndef.onreading = event => {
                 const decoder = new TextDecoder();
+                // for (const record of event.message.records) {
+                //   var data = decoder.decode(record.data);
+                //   var persedData = JSON.parse(data);
+                //   this.consoleLog("TOTOTITITATA");
+                //   this.consoleLog(data);
+                //   this.consoleLog(persedData);
                 for (const record of event.message.records) {
-                  var data = decoder.decode(record.data);
-                  var persedData = JSON.parse(data);
-                  this.consoleLog("TOTOTITITATA");
-                  this.consoleLog(data);
-                  this.consoleLog(persedData);
+                    consoleLog("Record type:  " + record.recordType);
+                    consoleLog("MIME type:    " + record.mediaType);
+                    consoleLog("=== data ===\n" + decoder.decode(record.data));
                 }
               }
             } catch(error) {
