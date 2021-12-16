7930df8c86151f49ae6b0b1b6496b2e55cda8a3a
Axelle HUMBERT <humbertaxelle40@gmail.com>
2021-12-16 14:52:28 +0100

    test data

--- src/App.vue
+++ src/App.vue
@@ -69,9 +69,10 @@ export default {
           try {
             await ndef.write({
               records: [
-                { recordType: "text", data: "name : Felix" },
-                { recordType: "text", data: "lastname: Dupond" },
-                { recordType: "text", data: "birthday: 22/01/2010" }
+                { recordType: "text", data: "name Felix" },
+                { recordType: "text", data: "lastname Dupond" },
+                { recordType: "text", data: "birthday 22/01/2010" },
+                { recordType: "text", data: "phone 0659483729" }
               ],
             });
             this.consoleLog("NDEF message written!");
