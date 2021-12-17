3558617bc3ee019ee35c901612006d8b255e9a2b
MaximeVerger <vergermaxime.sio@gmail.com>
2021-12-17 20:22:40 +0100

    ??

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,72 @@
 <template>
-  <button v-on:click="askPermission" id="nfc">Permission NFC</button>
-  <p>
-      <button v-on:click="readTag">Test NFC Read</button>
-      <button v-on:click="writeTag">Test NFC Write</button>
-  </p>
-  <pre id="name"></pre>
-  <pre id="lastname"></pre>
-  <pre id="birthday"></pre>
-  <pre id="phone"></pre>
 
-  <form @submit.prevent="writeTag">
-    <label for="name">Nom de l'animale : </label>
-    <input id="name" required v-model="newName" />
-    <label for="masterName">Nom de famille : </label>
-    <input id="masterName" required v-model="masterName" />
-    <label for="birthday">Date de naissance : </label>
-    <input id="birthday" required v-model="birthday" />
-    <label for="phone">Téléphone du propriétaire : </label>
-    <input id="phone" required v-model="phone" />
-    <button>Ajouter</button>
-  </form>
+<header>
+    <h1>Master finder</h1>
+    <img src="" alt="Logo"/>
+</header>
 
-  <pre id="log"></pre>
+<main>
+    <h2>Présentation</h2>
+    <article>
+        <p>
+            Ce site a pour fonction de vous aider à retrouver le propriétaire d'un animale, 
+            et de vous permettre d'entrer vos informations et celle de l'animale sur une puce NFC
+            pour pouvoir vous joindre si quelqu'un d'autre le trouve.
+            ATTENTION : Ce site ne fonctionne qu'avec le navigateur Chrome !
+        </p>
+    </article>
+
+    <h2>Lire la puce d'un animale</h2>
+    <article>
+        <label>Il faut appuyer puis placer la puce derrière votre smartphone</label>
+        <button v-on:click="readTag">Lire</button>
+    </article>
+
+    <h2>Ecrire sur la puce d'un animale</h2>
+    <article>
+        <label>Il faut appuyer puis placer la puce derrière votre smartphone</label>
+        <label>Ne surtout pas écrire sur la puce d'un animale qui n'est pas le votre !</label>
+        <form @submit.prevent="writeTag">
+            <label for="name">Nom de l'animale : </label>
+            <input id="name" required v-model="newName" />
+            <label for="masterName">Nom de famille : </label>
+            <input id="masterName" required v-model="masterName" />
+            <label for="birthday">Date de naissance : </label>
+            <input id="birthday" required v-model="birthday" />
+            <label for="phone">Téléphone du propriétaire : </label>
+            <input id="phone" required v-model="phone" />
+            <button>Ecrire</button>
+        </form>
+        <!-- <button v-on:click="readTag">Ecrire</button> -->
+    </article>
+
+    <h2>Lecture de puce NFC</h2>
+    <article>
+        <label>Veuillez prendre en compte les faits suivants : </label>
+        <ul>
+            <li> Votre smartphone ne peut pas forcément lire les puces NFC </li>
+            <li> Vous devez utiliser le navigateur Chrome </li>
+            <li><ul> Pour activer la permission de votre téléphone en cas de problème</ul></li>
+                <li>Essayez de l'activer manuellement sur votre téléphone</li>
+                <li>Essayez d'appuyer sur le bouton "Permission NFC" ci dessous</li>
+        </ul>
+        <button v-on:click="askPermission" id="nfc">Permission NFC</button>
+    </article>
+
+    <!-- <p>
+        <button v-on:click="writeTag">Test NFC Write</button>
+    </p> -->
+
+    <pre id="name"></pre>
+    <pre id="lastname"></pre>
+    <pre id="birthday"></pre>
+    <pre id="phone"></pre>
+    <pre id="log"></pre>
+</main>
+
+<footer>
+</footer>
+
 </template>
 
 <script>
